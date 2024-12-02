da88c7fd66ee24400a94093ec389494e8290dd7c
Jduun <isaevstanislav2003@mail.ru>
2024-12-03 01:54:17 +0300

    chore: minor changes in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,30 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { LoginForm } from './pages/LoginForm'
 import { SignUpForm } from './pages/SignUpForm'
-import { ProblemSet } from './pages/ProblemSet'
+import { ProblemList } from './pages/ProblemList'
+import { ProblemPage } from './pages/ProblemPage'
+import { ThemeProvider } from "@/components/ui/theme-provider"
+import { ContestList } from './pages/ContestList'
+import { ContestPage } from './pages/ContestPage'
+import { Toaster } from "@/components/ui/sonner"
 
 function App() {
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ProblemSet />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/signup" element={<SignUpForm />} />
-      </Routes>
-    </Router>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<ProblemList />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/signup" element={<SignUpForm />} />
+            <Route path="/problems/:id" element={<ProblemPage />} />
+            <Route path="/contests" element={<ContestList />} />
+            <Route path="/contests/:id" element={<ContestPage />} />
+          </Routes>
+        </Router>
+        <Toaster />
+      </div>
+    </ThemeProvider>
   );
 }
 
