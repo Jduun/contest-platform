b71bc39d4abf76b8027710bbf35a8e2f5559a0ec
Jduun <isaevstanislav2003@mail.ru>
2025-02-26 15:17:50 +0300

    comment remove data commands

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-sudo rm -r "${DB_PATH}" "${MINIO_PATH}"
+# Uncomment this if you want to delete all data
+# if [ -f .env ]; then
+#   set -a
+#   . ./.env
+#   set +a
+# fi
+# sudo rm -r "${DB_PATH}" "${MINIO_PATH}"
+
 docker compose down
 docker compose up --build
